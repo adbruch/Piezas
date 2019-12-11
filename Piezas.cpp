65cfd10f5759be7f5c5446e828255071ab5317b5
Austin D Bruch <adbruch@ecc-jaguar.csuchico.edu>
2019-12-11 15:33:19 -0800

    Added extra braces to constructor

--- Piezas.cpp
+++ Piezas.cpp
@@ -24,8 +24,12 @@ Piezas::Piezas()
 {
     turn = X;
     for (int i = 0; i < BOARD_ROWS; i++)
+    {
         for (int j = 0; j < BOARD_COLS; j++)
+        {
             board[i][j] = Blank;
+        }
+    }
 }
 
 // Toggle turn
