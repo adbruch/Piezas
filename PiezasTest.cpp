569aeac109f9d8adb0e6bd929fd15ef77f6fce6f
Austin D Bruch <adbruch@ecc-jaguar.csuchico.edu>
2019-12-12 11:49:31 -0800

    Added reset, pieceAt, and gamestate tests

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -72,5 +72,138 @@ TEST(PiezasTest, dropPieceRandom)
 	pieza.dropPiece(3);
 	pieza.dropPiece(0);
 	pieza.dropPiece(0);
-	ASSERT_EQ( pieza.dropPiece(2), _XSTRING_);
+	ASSERT_EQ( pieza.dropPiece(2), X);
+}
+//reset test-----------------------
+
+TEST(PiezasTest, resetTest)
+{
+	Piezas pieza;
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.reset();
+	ASSERT_EQ( pieza.pieceAt(0,2), Blank);
+}
+//pieceAt tests-----------------------------------------------
+TEST(PiezasTest, pieceAtRandom)
+{
+	Piezas pieza;
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(3);
+	pieza.dropPiece(1);
+	pieza.dropPiece(3);
+	pieza.dropPiece(0);
+	pieza.dropPiece(0);
+	ASSERT_EQ( pieza.pieceAt(0,0), X);
+}
+
+TEST(PiezasTest, pieceAtRandom2)
+{
+	Piezas pieza;
+	pieza.dropPiece(1);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(3);
+	pieza.dropPiece(1);
+	pieza.dropPiece(3);
+	pieza.dropPiece(0);
+	pieza.dropPiece(0);
+	ASSERT_EQ( pieza.pieceAt(1,3), O);
+}
+
+TEST(PiezasTest, PieceAtInvalid)
+{
+	Piezas pieza;
+	pieza.dropPiece(0);
+	ASSERT_EQ( pieza.dropPiece(-1,4), Invalid);
+}
+
+TEST(PiezasTest, PieceAtBlank)
+{
+	Piezas pieza;
+	pieza.dropPiece(0);
+	ASSERT_EQ( pieza.dropPiece(1,1), Blank);
+}
+
+//gameState tests------------------------------
+TEST(PiezasTest, gameStateInvalid)
+{
+	Piezas pieza;
+	pieza.dropPiece(0);
+	ASSERT_EQ( pieza.gameState(), Invalid);
+}
+
+TEST(PiezasTest, gameStateInvalid2)
+{
+	Piezas pieza;
+	pieza.dropPiece(0);
+	pieza.dropPiece(0);
+	pieza.dropPiece(0);
+	pieza.dropPiece(1);
+	pieza.dropPiece(1);
+	pieza.dropPiece(1);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(3);
+	pieza.dropPiece(3);
+	ASSERT_EQ( pieza.gameState(), Invalid);
+}
+
+TEST(PiezasTest, gameStateTie)
+{
+	Piezas pieza;
+	pieza.dropPiece(0);
+	pieza.dropPiece(0);
+	pieza.dropPiece(0);
+	pieza.dropPiece(1);
+	pieza.dropPiece(1);
+	pieza.dropPiece(1);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(3);
+	pieza.dropPiece(3);
+	pieza.dropPiece(3);
+	ASSERT_EQ( pieza.gameState(), Blank);
+}
+
+TEST(PiezasTest, gameStateX)
+{
+	Piezas pieza;
+	pieza.dropPiece(0);
+	pieza.dropPiece(1);
+	pieza.dropPiece(0);
+	pieza.dropPiece(2);
+	pieza.dropPiece(0);
+	pieza.dropPiece(3);
+	pieza.dropPiece(1);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(1);
+	pieza.dropPiece(3);
+	pieza.dropPiece(3);
+	ASSERT_EQ( pieza.gameState(), X);
+}
+
+TEST(PiezasTest, gameStateO)
+{
+	Piezas pieza;
+	pieza.toggleTurn();
+	pieza.dropPiece(0);
+	pieza.dropPiece(1);
+	pieza.dropPiece(0);
+	pieza.dropPiece(2);
+	pieza.dropPiece(0);
+	pieza.dropPiece(3);
+	pieza.dropPiece(1);
+	pieza.dropPiece(2);
+	pieza.dropPiece(2);
+	pieza.dropPiece(1);
+	pieza.dropPiece(3);
+	pieza.dropPiece(3);
+	ASSERT_EQ( pieza.gameState(), O);
 }
